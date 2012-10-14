43809a50c71e9d645bfb921727bb0f04d36626c8
kirkstrobeck <kirk@strobeck.com>
2012-10-14 12:31:13 -0700

    updates

--- workspace/themes/active/_README.md
+++
@@ -1,3 +0,0 @@
-# Theme: Athey Creek
-
-This theme is used by Athey Creek church at http://atheycreek.com
--- workspace/themes/active/coffee/_build.sh
+++
@@ -1,4 +0,0 @@
-
-echo "Coffee brewing .."
-
-coffee --join common.js --lint --output ../js --watch --compile common.coffee # add other files here
