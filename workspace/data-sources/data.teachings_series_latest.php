b0fac77f6336ed6c06136be9f5e4ea55e5ce9584
jsimcoe <jdsimcoe@gmail.com>
2013-08-18 11:34:38 -0700

    added datasource to keep from erroring out

---
+++ workspace/data-sources/data.teachings_series_latest.php
@@ -0,0 +1,74 @@
+<?php
+
+	require_once(TOOLKIT . '/class.datasource.php');
+
+	Class datasourceteachings_series_latest extends SectionDatasource {
+
+		public $dsParamROOTELEMENT = 'teachings-series-latest';
+		public $dsParamConditionalizer = '(if value of ({$pt1}) is ())';
+		public $dsParamORDER = 'asc';
+		public $dsParamPAGINATERESULTS = 'yes';
+		public $dsParamLIMIT = '1';
+		public $dsParamSTARTPAGE = '1';
+		public $dsParamREDIRECTONEMPTY = 'no';
+		public $dsParamSORT = 'order';
+		public $dsParamASSOCIATEDENTRYCOUNTS = 'no';
+		
+
+		public $dsParamFILTERS = array(
+				'233' => 'no',
+		);
+		
+
+		public $dsParamINCLUDEDELEMENTS = array(
+				'title: formatted'
+		);
+		
+
+		public function __construct($env=NULL, $process_params=true) {
+			parent::__construct($env, $process_params);
+			$this->_dependencies = array();
+		}
+
+		public function about() {
+			return array(
+				'name' => 'Teachings: Series: Latest',
+				'author' => array(
+					'name' => 'Jonathan Simcoe',
+					'website' => 'http://atheycreek.dev',
+					'email' => 'jdsimcoe@gmail.com'),
+				'version' => 'Symphony 2.3.2',
+				'release-date' => '2013-08-16T22:33:08+00:00'
+			);
+		}
+
+		public function getSource() {
+			return '27';
+		}
+
+		public function allowEditorToParse() {
+			return true;
+		}
+
+		public function execute(array &$param_pool = null) {
+			$result = new XMLElement($this->dsParamROOTELEMENT);
+
+			try{
+				$result = parent::execute($param_pool);
+			}
+			catch(FrontendPageNotFoundException $e){
+				// Work around. This ensures the 404 page is displayed and
+				// is not picked up by the default catch() statement below
+				FrontendPageNotFoundExceptionHandler::render($e);
+			}
+			catch(Exception $e){
+				$result->appendChild(new XMLElement('error', $e->getMessage()));
+				return $result;
+			}
+
+			if($this->_force_empty_result) $result = $this->emptyXMLSet();
+
+			return $result;
+		}
+
+	}
