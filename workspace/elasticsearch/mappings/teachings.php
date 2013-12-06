f2bf27685d04f82eae17163a17701f4f1982ca32
jdsimcoe <jonathan@simko.io>
2013-12-06 14:38:45 -0800

    updateS

---
+++ workspace/elasticsearch/mappings/teachings.php
@@ -0,0 +1,20 @@
+<?php
+
+  class elasticsearch_teachings {
+
+    public function mapData(Array $data, Entry $entry) {
+      $json = array();
+      // var_dump($data);
+
+      $json['_boost'] = 1;
+
+      $json['title'] = $data['title']['value'];
+      $json['book'] = $data['book']['value'];
+      $json['chapter'] = $data['chapter']['value'];
+      $json['description'] = $data['description']['value'];
+      $json['speaker'] = $data['speaker']['value'];
+
+      return $json;
+    }
+
+  }
